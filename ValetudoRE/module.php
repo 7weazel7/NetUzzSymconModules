a124a7a8523bbc0a54eb506749852bbf2622a93b
Florian Uetz <uetzfl@MacBookAir-Florian.ad.netuzz.de>
2020-09-05 11:04:55 +0200

    s

--- ValetudoRE/module.php
+++ ValetudoRE/module.php
@@ -147,8 +147,7 @@ require_once __DIR__ . '/../libs/helper/HELP_ValetudoRE.php';
                 if (property_exists($Payload, 'valetudo_state')) {
                     $this->SendDebug('*attributes Payload', print_r($Payload, true), 0);
                     $this->SendDebug('*attributes $Payload->valetudo_state', print_r($Payload->valetudo_state, true), 0);
-                    $valetudo_state = json_decode($Payload->valetudo_state);
-                    $this->SendDebug('*attributes valetudo_state->id', print_r($Payload['valetudo_state'][0]['id'], true), 0);
+                    $this->SendDebug('*attributes $Payload->valetudo_state->id', print_r($Payload->valetudo_state->id, true), 0);
                     #$this->SetValue('VRE_State', $ValetudoState->id);
                 }
                 /*
